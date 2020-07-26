e38a3ddcdd112ba44a5a43aae299e4032edef691
davidkvara <davekvaratskhelia@gmail.com>
2020-07-26 19:09:08 +0400

    add client

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="container">
+          <h2>front for the back</h2>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
